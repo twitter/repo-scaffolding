e87b24b642fdd32ccc7c168e7f1ee9747c1387e4
Himanshu Mishra <hmishra@twitter.com>
2018-08-09 14:02:36 -0700

    Add license headers

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+"""
+Copyright 2018 Twitter, Inc.
+Licensed under the Apache License, Version 2.0
+http://www.apache.org/licenses/LICENSE-2.0
+"""
 
 from setuptools import setup
 
